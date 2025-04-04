f20fb9e03436c03f054806b9653e55a00c09359e
m02542 <fredrik.neby@skatteetaten.no>
2025-04-04 14:55:47 +0200

    removed console.log

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import ResponsiveMenu from "./ResponsiveMenu";
 
 export const Header = () => {
   return (
-    <header className="py-6 text-center w-full">
+    <header className="py-6 text-center w-full bg-wow-dark">
       <h1 className="text-4xl font-bold">Nerdelandslaget</h1>
       <ResponsiveMenu />
     </header>
--- src/components/ResponsiveMenu.tsx
+++ src/components/ResponsiveMenu.tsx
@@ -38,32 +38,29 @@ const ResponsiveMenu = () => {
     selected.toLowerCase() === item.toLowerCase();
 
   return (
-    <nav className="bg-wow-dark p-4 w-full">
+    <nav className="p-4 w-full">
       <ul className="flex items-center justify-center flex-wrap gap-2">
-        {menuItems.map((item) => {
-          console.log("SELECTED", isSelected(item.route), selected, item.route);
-          return (
-            <li
-              key={item.name}
-              className={clsx("m-2", isSelected(item.route) && "bg-gray-700")}
+        {menuItems.map((item) => (
+          <li
+            key={item.name}
+            className={clsx("m-2", isSelected(item.route) && "bg-gray-700")}
+          >
+            <Link
+              to={item.route}
+              className="flex flex-row items-center space-x-2 border border-gray-700 rounded p-2 hover:bg-wow-dark hover:text-wow-gold"
             >
-              <Link
-                to={item.route}
-                className="flex flex-row items-center space-x-2 border border-gray-700 rounded p-2 hover:bg-wow-dark hover:text-wow-gold"
-              >
-                <img
-                  src={item.image}
-                  alt={item.alt}
-                  className="w-8 h-8 object-contain"
-                  loading="lazy"
-                />
-                <span className="text-sm md:text-base font-medium">
-                  {item.name}
-                </span>
-              </Link>
-            </li>
-          );
-        })}
+              <img
+                src={item.image}
+                alt={item.alt}
+                className="w-8 h-8 object-contain"
+                loading="lazy"
+              />
+              <span className="text-sm md:text-base font-medium">
+                {item.name}
+              </span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
